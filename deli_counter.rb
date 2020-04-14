50bc33d77d1a47e17d9b368c32a4f70ab9bafa18
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-14 16:46:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,9 @@
-def line 
-  
+def line(people_in_line)
+  if people_in_line.length == 0
+    "The line is currently empty."
+  else 
+    index = 0
+    message = ""
+    while index < people_in_line.length
+      
 end 
