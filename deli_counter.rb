42c2beafbe29b44f9eaefe567ac2daaab0f55b6f
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-14 16:56:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(people_in_line)
   if people_in_line.length == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else 
     message = "The line is currently:"
     index = 0
@@ -8,6 +8,6 @@ def line(people_in_line)
       message = "#{message} #{index + 1}. #{people_in_line[index]}"
       index += 1
     end
+    puts message
   end
-  return message
 end 
